82bc936e261fcab07899ae1538c7fb6669c3ee17
clayton.campos <clayton.almeida.campos@gmail.com>
2024-07-03 17:14:18 -0300

    feat: add scrapy ml

--- coleta_ml/coleta_ml/settings.py
+++ coleta_ml/coleta_ml/settings.py
@@ -14,7 +14,7 @@ NEWSPIDER_MODULE = "coleta_ml.spiders"
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = "coleta_ml (+http://www.yourdomain.com)"
+USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36"
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
